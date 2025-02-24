a41b0f7a69fac7b3236df6755cf4a2fc97a1f53f
baseergroot <githubbs7@gmail.com>
2025-02-24 08:40:19 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const { alldl } = require("rahad-all-downloader");
+
+const app = express();
+app.use(cors());
+app.use(express.json()); // Middleware to parse JSON body
+
+// app.use(express.static('dist'))
+
+app.post("/download", async (req, res) => {
+    console.log("req.check: ", req.body)
+    const { url } = req.body; // Get URL from request body
+    if (!url) return res.status(400).json({ error: "URL is required" });
+
+    try {
+        const data = await alldl(url); // Use alldl function
+        res.json(data);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).json({ error: "Failed to fetch video", details: err.message });
+    }
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
