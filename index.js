06fa8af81d040d7bf4a50800e6d50d717da5ba7a
baseergroot <githubbs7@gmail.com>
2025-03-16 13:59:30 +0500

    updated for vercel deployment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { alldl } = require("rahad-all-downloader");
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
@@ -25,4 +26,8 @@ app.post("/download", async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const port = process.env.PORT || 3000;
+
+if (process.env.VERCEL !== "1") {
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+}
